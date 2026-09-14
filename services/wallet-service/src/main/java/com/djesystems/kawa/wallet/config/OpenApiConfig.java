362f4216de5f672820b8c860c3e74e9e498544c7
kawa <kawadje78@gmail.com>
2026-09-14 21:45:25 +0200

    secure retailer service swagger walet

---
+++ services/wallet-service/src/main/java/com/djesystems/kawa/wallet/config/OpenApiConfig.java
@@ -0,0 +1,29 @@
+package com.djesystems.kawa.wallet.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+    info = @Info(
+        title = "KAWA Wallet API",
+        version = "v1",
+        description = "KAWA Wallet API for retailer integration"
+    ),
+    security = {
+        @SecurityRequirement(name = "bearerAuth")
+    }
+)
+@SecurityScheme(
+    name = "bearerAuth",
+    type = SecuritySchemeType.HTTP,
+    scheme = "bearer",
+    bearerFormat = "JWT"
+)
+public class OpenApiConfig {
+}
